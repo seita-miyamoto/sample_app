68961feb362336876e63b8f17ea163652854cff9
EC2 Default User <ec2-user@ip-172-31-17-45.us-east-2.compute.internal>
2020-06-03 13:49:56 +0000

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,7 +12,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
     # assert_select 'div#<CSS id for error explanation>'
-    # assert_select 'div.<CSS class for field with error>'
+    # assert_select 'div.<CSS class for field with error>'cd
   end
   
   test "valid signup information" do
